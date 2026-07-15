949a8bd307d9d136cbc46bad5d3cb101db905d3a
itfeelsnice <berceabercea091234@gmail.com>
2026-07-15 11:12:54 +0300

    this thing with the design is killing me

---
+++ python1.py
@@ -0,0 +1,11 @@
+# from flask import Flask, render_template
+# import time
+
+# start1 = Flask(__name__)
+
+# @start1.route("/homepage")
+# def def1():
+#     return render_template("html1.html")
+
+
+
