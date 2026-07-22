eddf47066da17ffa2c57f178b8e448ed1cd4c1e4
itfeelsnice <berceabercea091234@gmail.com>
2026-07-22 18:35:20 +0300

    i dont know what to write here right now the things just going well

--- static/js1.js
+++ static/js1.js
@@ -1,3 +1,11 @@
 
+let tell1 = document.querySelector("#tell1");
 
+let look1 = new IntersectionObserver((watch1) =>{
+    let watch2 = watch1[0];
+    if (watch2.isIntersecting){
+        tell1.classList.add("opac1");
+    }
+});
 
+look1.observe(tell1);
